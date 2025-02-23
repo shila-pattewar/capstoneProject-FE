7cd02df6920145755142ea5de5d33ffbc4c01a22
shila <shilapattewar16@gmail.com>
2025-02-23 14:08:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+//componets
+import HomePage from './component/HomePage'
+import Header from './component/Header'
+import Footer from './component/Footer'
+import ProductPage from './component/Productpage'
+import ProductDetails from './component/ProductDetails'
+import PlaceOrder from './component/PlaceOrder'
+import Confirmation from './component/Confirmation'
+import Cart from './component/Cart'
+
+function App(){
+  const[count, setCount] = useState(0)
+  return(
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/products' element={<ProductPage/>} />
+        <Route path='/products/:id' element={<ProductDetails/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/placeorder' element={<PlaceOrder/>} />
+        <Route path='/confirmation' element={<Confirmation/>} />
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Routes>
+  )
+}
+
+export default App
