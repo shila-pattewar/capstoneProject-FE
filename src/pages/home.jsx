6a4e45b4a35a6391689128dff70d2509aa2652dd
shila <shilapattewar16@gmail.com>
2025-02-26 13:06:17 -0600

    updated home text and added css for bakes4u word

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home(){
         <div className='home-bg'>
             <h1>Welcome to Bakes4u</h1>
             <p>
-                At Sweet Treats Bakery, we specialize in handmade, fresh baked goods made with love. Our team of expert bakers work hard to create delicious treats that will make every occasion sweeter.
+                At <span>Bakes4u</span> Bakery, we specialize in handmade, fresh baked goods made with love. Our team of expert bakers work hard to create delicious treats that will make every occasion sweeter.
                 Whether you're celebrating a special event or just indulging yourself, we have something for everyone. We use only the best ingredients to ensure our products are top-notch in both flavor and quality.<br/><br/>
                 - Fluffy and soft bread rolls that are perfect for any meal, from sandwiches to dinner rolls.<br/>
                 - A refreshing pie with a tangy lemon filling, topped with a fluffy meringue.<br/>
